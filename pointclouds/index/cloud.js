be594d9a60058cb4beae2f2b030e9e8e84dae1f6
ambarja <antony.barja8@gmail.com>
2023-09-28 09:04:33 -0500

    up

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 2548495,
+    "boundingBox": {
+        "lx": 667860.29,
+        "ly": 9532686.6,
+        "lz": 2.67,
+        "ux": 669177.4000000013,
+        "uy": 9534003.71,
+        "uz": 1319.7800000012667
+    },
+    "tightBoundingBox": {
+        "lx": 667860.29,
+        "ly": 9532686.6,
+        "lz": 2.67,
+        "ux": 669040.0,
+        "uy": 9534003.71,
+        "uz": 153.27
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 11.40650749206543,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
